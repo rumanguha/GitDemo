4772766180e6ae05e78146bc0ea025b47bd89369
Ruman <guhaniyogi.ruman@gmail.com>
2022-09-22 18:44:12 +0530

    first commit

---
+++ src/test/java/RGN/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,43 @@
+package RGN.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config()
+	{
+		// ExtentReports, ExtentSparkReporter
+		String path = System.getProperty("user.dir")+ "\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "RGN");
+	}
+	
+	@Test
+	public void initialDemo()
+	{
+		ExtentTest test = extent.createTest("initialDemo");
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://rahulshettyacademy.com/client/");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result do not match");
+		extent.flush();
+	}
+}
